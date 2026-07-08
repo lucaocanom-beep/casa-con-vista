0e9c804e28e6eb2ac7a9fb284c67ad5d93c87585
Vincenzo Cognome <luca.ocanom@gmail.com>
2026-07-08 17:41:51 +0200

    Aggiungi Meta Pixel (1028679406276713)

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -83,6 +83,16 @@ export default async function LocaleLayout({ children, params }: Props) {
           gtag('js', new Date());
           gtag('config', 'G-GCT99ZSCQV');
         `}</Script>
+        <Script id="meta-pixel" strategy="afterInteractive">{`
+          !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+          n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}
+          (window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init','1028679406276713');
+          fbq('track','PageView');
+        `}</Script>
+        <noscript>{`<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1028679406276713&ev=PageView&noscript=1"/>`}</noscript>
         {/* Skip link: visibile solo quando riceve focus (utenti tastiera/screen reader) */}
         <a
           href="#main"
